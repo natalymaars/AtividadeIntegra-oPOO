1a1cdfb8985c426f5de6f33f333d61f5d3af1448
Nataly Marçal <111927457+natalymaars@users.noreply.github.com>
2023-11-23 15:24:06 -0300

    Add files via upload

---
+++ AtividadeIntegraçao/src/reservaPassagens/Passageiro.java
@@ -0,0 +1,5 @@
+package reservaPassagens;
+
+public class Passageiro {
+
+}
---
+++ AtividadeIntegraçao/src/reservaPassagens/Voo.java
@@ -0,0 +1,14 @@
+package reservaPassagens;
+import java.util.Date;
+
+public class Voo {
+	public String FlightNumber;
+	public String Origin;
+	public String Destiny;
+	public Date FlightDateAndTime;
+	public int TotalSeats;
+	public int AvailableSeats;
+	
+	
+
+}
