b49a881b93616ff98c2bffb10d3cbde5a8d1d2c1
Nataly Marçal <111927457+natalymaars@users.noreply.github.com>
2023-11-24 11:46:43 -0300

    Update Voo.java

--- AtividadeIntegraçao/src/reservaPassagens/Voo.java
+++ AtividadeIntegraçao/src/reservaPassagens/Voo.java
@@ -2,12 +2,76 @@ package reservaPassagens;
 import java.util.Date;
 
 public class Voo {
-	public String FlightNumber;
-	public String Origin;
-	public String Destiny;
-	public Date FlightDateAndTime;
-	public int TotalSeats;
-	public int AvailableSeats;
+	private String flightNumber;
+	private String origin;
+	private String destination;
+	private Date flightDateAndTime;
+	private int totalSeats;
+	private int availableSeats;
+	
+	public Voo(String flightNumber, String origin, String destination, Date flightDateAndTime, int totalSeats) {
+		this.flightNumber = flightNumber;
+		this.origin = origin;
+		this.destination = destination;
+		this.flightDateAndTime = flightDateAndTime;
+		this.totalSeats= totalSeats;
+		
+		
+	}
+	
+	public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String newFlightNumber) {
+        this.flightNumber = newFlightNumber;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String newOrigin) {
+        this.origin = newOrigin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String newDestination) {
+       this.destination = newDestination;
+    }
+
+    public Date getFlightDateAndTime() {
+        return flightDateAndTime;
+    }
+
+    public void setFlightDateAndTime(Date newFlightDateAndTime) {
+    	this.flightDateAndTime = newFlightDateAndTime;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public void setTotalSeats(int newTotalSeats) {
+    	this.totalSeats = newTotalSeats;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int newAvailableSeats) {
+    	this.availableSeats = newAvailableSeats;
+    }
+	
+	
+	
+	
+	
+	
 	
 	
 
